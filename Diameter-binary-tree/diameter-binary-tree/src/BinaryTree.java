b6a925fe85cd8e96bf654da6b9852be94a9d7a56
jawad abo fakher <aboufakherj@gmail.com>
2023-12-27 22:00:21 +0200

    adding the algorithm that calculates the diameter of a binary tree

---
+++ Diameter-binary-tree/diameter-binary-tree/src/BinaryTree.java
@@ -0,0 +1,88 @@
+
+
+public class BinaryTree {
+
+    static class TreeNode {
+        int data;
+        TreeNode left, right;
+        public TreeNode(int data) {
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
+
+    public TreeNode addNode(TreeNode root, int data) {
+        if (root == null) {
+            return new TreeNode(data);
+        }
+        if (root.data < data) {
+            root.right = addNode(root.right, data);
+        } else if ( root.data > data) {
+            root.left = addNode(root.left, data);
+        }
+        return root;
+    }
+
+    public TreeNode find(TreeNode root, int data) {
+        if (data == root.data) {
+            return root;
+        }
+
+        if (root.data > data) {
+            find(root.left, data);
+        } else {
+            find(root.right, data);
+        }
+        return null;
+    }
+
+    public void inOrderTraversal(TreeNode root) {
+        if (root != null) {
+            inOrderTraversal(root.left);
+            System.out.print(root.data + " ");
+            inOrderTraversal(root.right);
+        }
+    }
+
+    public int treeDiameter(TreeNode root) {
+        int[] max = new int[1];
+        treeDiameterHelper(root, max);
+        return max[0];
+    }
+    public int treeDiameterHelper(TreeNode root, int[] max) {
+        if (root == null) {
+            return 0;
+        }
+        int left = treeDiameterHelper(root.left, max);
+        int right = treeDiameterHelper(root.right, max);
+        max[0] = Math.max(max[0], left + right);
+        return 1 + Math.max(left, right);
+    }
+
+
+    public static void main(String[] args) {
+        /*
+                        11
+                      /   \
+                    2       70
+                   / \     / \
+                 -1   4   66   77
+                 /   / \
+               -8   3   9
+        */
+        TreeNode root = new TreeNode(11);
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.addNode(root,2);
+        binaryTree.addNode(root,70);
+        binaryTree.addNode(root,4);
+        binaryTree.addNode(root,3);
+        binaryTree.addNode(root,-1);
+        binaryTree.addNode(root,-8);
+        binaryTree.addNode(root,9);
+        binaryTree.addNode(root,77);
+        binaryTree.addNode(root,66);
+        binaryTree.inOrderTraversal(root);
+        System.out.println();
+        System.out.print("The diameter is : " + binaryTree.treeDiameter(root));
+    }
+}
